04d518fc32d17b5f55828644712a1342f136f9d2
Manu Bharadwaj BN <102406248+Manu577228@users.noreply.github.com>
2024-01-03 18:55:16 +0530

    made changes in index.js

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-const inputs = document.querySelectorAll("input");
-const button = document.querySelector("button");
+const inputs = document.querySelectorAll("input"),
+  button = document.querySelector("button");
 
-inputs.forEach((input, index) => {
-  input.addEventListener("input", () => {
+inputs.forEach((input, index1) => {
+  input.addEventListener("keyup", (e) => {
     const currInput = input;
-    const nextInput = input.nextElementSibling;
-    const prevInput = input.previousElementSibling;
+    (nextInput = input.nextElementSibling),
+      (prevInput = input.previousElementSibling);
 
     if (currInput.value.length > 1) {
-      currInput.value = currInput.value.charAt(0);
+      currInput.value = "";
       return;
     }
 
-    if (nextInput && currInput.value !== "") {
+    if (
+      nextInput &&
+      nextInput.hasAttribute("disabled") &&
+      currInput.value !== ""
+    ) {
       nextInput.removeAttribute("disabled");
       nextInput.focus();
     }
 
-    if (
-      prevInput &&
-      e.inputType === "deleteContentBackward" &&
-      currInput.value === ""
-    ) {
-      prevInput.focus();
+    if (e.key === "Backspace") {
+      inputs.forEach((input, index2) => {
+        if (index1 <= index2 && prevInput) {
+          input.setAttribute("disabled", true);
+          currInput.value = "";
+          prevInput.focus();
+        }
+      });
     }
-
-    updateButtonState();
+    if (!inputs[3].disabled && inputs[3].value !== "") {
+      button.classList.add("active");
+      return;
+    }
+    button.classList.remove("active");
   });
 });
-
-function updateButtonState() {
-  const filledInputs = Array.from(inputs).filter((input) => input.value !== "");
-  if (filledInputs.length === 4) {
-    button.classList.add("active");
-  } else {
-    button.classList.remove("active");
-  }
-}
 
 window.addEventListener("load", () => inputs[0].focus());
